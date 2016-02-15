a8bfe513dbab841f895bf2dc38409ce077e53223
Quixatocs <quixatocs@gmail.com>
2016-02-15 22:01:10 +0000

    ScoreManager class added
    Major
    -score manager class added
    -initial placeholder methods added

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ScoreManager : MonoBehaviour {
+
+	Dictionary<string, Dictionary<string, int> > scores;
+
+	void Start () {
+	
+	}
+
+	public int getScore(string username, string scoreType) {
+		return null;
+	}
+
+	public void setScore(string username, string scoreType, int value) {
+		return null;
+	}
+
+	public void changeScore(string username, string scoreType, int amount) {
+		return null;
+	}
+
+}
