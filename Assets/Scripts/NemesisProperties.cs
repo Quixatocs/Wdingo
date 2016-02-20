9da931d223d9514befa9ad91fe99b15cade7f8ba
Quixatocs <quixatocs@gmail.com>
2016-02-20 22:16:25 +0000

    Basic AI
    MAJOR
    -Grim follows player at exact same speed
    MINOR
    -created nemesis properties which all grim movement is based off, like
    player properties
    FIXES
    -need to make tweeks to nemesis movements
    -add some change in the grim’s speed

---
+++ Assets/Scripts/NemesisProperties.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NemesisProperties : MonoBehaviour{
+
+	//various changable attributes
+	private bool collided;
+
+	// player attributes
+	private float maxMovementVelocity = 2.5f;
+	private float stunnedTime = 2.0f;
+	private int distanceRebounded = 20;
+
+
+	// constants
+	private const float MOVEMENT_SMOOTH_TIME = 0.5f; 
+
+
+
+	//Getters and Setters
+	public void setMaxMovementVelocity(float newMax) {
+		this.maxMovementVelocity = newMax;
+	}
+
+	public float getMaxMovementVelocity() {
+		return maxMovementVelocity;
+	}
+
+	//Getter and Setter for Collided bool 
+	public void setCollided(bool newCollided) {
+		this.collided = newCollided;
+	}
+
+	public bool isCollided() {
+		return this.collided;
+	}
+
+	public float getMovementSmoothTime() {
+		return MOVEMENT_SMOOTH_TIME;
+	}
+
+	public void setStunnedTime(float stunnedTime) {
+		this.stunnedTime = stunnedTime;
+	}
+
+	public float getStunnedTime() {
+		return stunnedTime;
+	}
+
+	public void setDistanceRebounded(int distance) {
+		this.distanceRebounded = distance;
+	}
+
+	public int getDistanceRebounded() {
+		return distanceRebounded;
+	}
+
+
+
+
+
+
+}
