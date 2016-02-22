9f3f7369d851dccce705e568ac617437938fbf2d
Quixatocs <quixatocs@gmail.com>
2016-02-22 14:01:42 +0000

    Wrong branch New scene
    MAJOR
    -added new scene

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+	
+
+	void Update () {
+
+		if (Input.GetMouseButtonUp(0)) {
+			Debug.Log("Load Chase Level");
+			Application.LoadLevel("TheChase");
+		}
+	
+	}
+}
